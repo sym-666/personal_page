c20dcd91b19d4b223efb362cb55130a90f26132c
shiyiminig <2183444689@qq.com>
2026-03-12 17:08:27 +0800

    feat: add link

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,13 +8,13 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'A sport tracker app',
-    description: `Track your sports activities and monitor your performance with our intuitive sport tracker app.`,
+    description: `Track your sports activities and monitor your performance with our intuitive sport tracker app.Link : https://puppygoapp.com/`,
     imgSrc: '/static/images/sport-tracker.png',
     href: '/projects/sport-tracker',
   },
   {
     title: 'AI Intelligent Drug Detection Platform',
-    description: `An AI-powered platform designed to detect and analyze drug interactions, providing insights for safer medication management.`,
+    description: `An AI-powered platform designed to detect and analyze drug interactions, providing insights for safer medication management. Link : https://sym-666.github.io/medicine/`,
     imgSrc: '/static/images/drug.png',
     href: '/projects/drug-detection',
   },
@@ -26,7 +26,7 @@ const projectsData: Project[] = [
   },
   {
     title: 'cbaldur——a Nuclear fusion computing platform',
-    description: `Cbaldur is a cutting-edge nuclear fusion computing platform designed to simulate and analyze fusion reactions, providing valuable insights for researchers and scientists in the field of nuclear energy.`,
+    description: `Cbaldur is a cutting-edge nuclear fusion computing platform designed to simulate and analyze fusion reactions, providing valuable insights for researchers and scientists in the field of nuclear energy. Link: https://cbaldur.fusim.cn/ `,
     imgSrc: '/static/images/cbaldur.png',
     href: '/projects/cbaldur',
   },
